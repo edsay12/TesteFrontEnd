13a5aeb3b60c325165562796405bcd7719978023
edsay12 <edvandearaujo2@hotmail.com>
2024-02-28 20:03:26 -0300

    Adicionando dados iniciais a tabela

--- src/app/fakedata/index.tsx
+++ src/app/fakedata/index.tsx
@@ -1,8 +1,26 @@
-export const TABLE_HEAD = ['Car', 'Next Reservation', 'Status','Rating','Actions'];
+export const TABLE_HEAD = [
+  "Car",
+  "Next Reservation",
+  "Status",
+  "Rating",
+  "Actions",
+];
+
 export const TABLE_ROWS = [
-  { name: 'John Michael', job: 'Manager', date: '23/04/18' },
-  { name: 'Alexa Liras', job: 'Developer', date: '23/04/18' },
-  { name: 'Laurent Perrier', job: 'Executive', date: '19/09/17' },
-  { name: 'Michael Levi', job: 'Developer', date: '24/12/08' },
-  { name: 'Richard Gran', job: 'Manager', date: '04/10/21' },
+  {
+    id: 1,
+    carImage: "https://example.com/car1.jpg",
+    NextReservation: "Jun 06 - junho 12",
+    status: "Available",
+    ratingNumber: 4.5,
+    modelo: "Toyota Corolla",
+  },
+  {
+    id: 2,
+    carImage: "https://example.com/car2.jpg",
+    NextReservation: "Jun 10 - junho 15",
+    status: "Not available",
+    ratingNumber: 3.8,
+    modelo: "Honda Civic",
+  },
 ];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import TableHeadElement from "@/components/table/TableHeadElement";
 import TableBody from "@/components/table/TableBody";
 import TableBodyElement from "@/components/table/TableBodyElement";
 import TableRow from "@/components/table/TableRow";
+import Image from "next/image";
+import carImage from "./assets/carImage.png";
 
 export default function DefaultTable() {
   return (
@@ -22,14 +24,32 @@ export default function DefaultTable() {
         <TableBody>
           {TABLE_ROWS.map((data, index, list) => {
             return (
-              <TableRow key={data.name}>
+              <TableRow key={data.id}>
                 <TableBodyElement
-                  key={data.name}
+                  key={data.id}
                   datalength={list.length}
                   index={index}
                 >
-                  asdasd
+                  <div className="flex gap-2 items-center">
+                    <div>
+                      <Image
+                        src={carImage}
+                        alt=""
+                        width={100}
+                        height={100}
+                        className="max-w-[48px]"
+                      ></Image>
+                    </div>
+                    <p className="text-gray-900 font-normal text-sm">
+                      {data.modelo}
+                    </p>
+                  </div>
                 </TableBodyElement>
+                <TableHeadElement>
+                  <p className="text-sm font-normal text-gray-500">
+                    {data.NextReservation}
+                  </p>
+                </TableHeadElement>
               </TableRow>
             );
           })}
